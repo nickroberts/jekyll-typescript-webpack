ed7bb799b3e59c69412db94c168c571b69075215
Travis CI <nobody@nobody.org>
2016-10-22 19:46:20 -0400

    Update deploy script

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -4,13 +4,13 @@ const logger = require('./logger');
 logger.info('Deploying to gh-pages.');
 if (env.TRAVIS_BRANCH === 'master') {
   const execSync = require("child_process").execSync;
-  let command = 'cd _site && ' +
-    'git config user.name "Travis CI" && ' +
-    'git config user.email "nobody@nobody.org" && ' +
-    'git init && ' +
-    'git add . && ' +
-    'git commit -m "Travis is pushing it...pushing it real good" && ' +
-    'git push --force --quiet "https://' + env.GITHUB_TOKEN + '@github.com/jekyll-typescript-webpack.git" master:gh-pages';
+  let command = `cd _site &&
+git config user.name "Travis CI"
+git config user.email "nobody@nobody.org"
+git init
+git add .
+git commit -m "Travis is pushing it...pushing it real good"
+git push --force --quiet "https://${env.GITHUB_TOKEN}@github.com/jekyll-typescript-webpack.git" master:gh-pages > /dev/null 2>&1`;
   execSync(command, {
     env: env,
     stdio: 'inherit'
