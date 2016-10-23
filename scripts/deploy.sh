447aa42b6c904c27534d7f74614605242f40dd99
Travis CI <nobody@nobody.org>
2016-10-22 20:21:17 -0400

    Update deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,8 +7,8 @@ git config --global user.email "nobody@nobody.org"
 git init
 git add .
 git commit -m "Travis is pushing it...pushing it real good :)"
-git push --force --quiet "https://${GITHUB_TOKEN}@github.com/jekyll-typescript-webpack.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/nickroberts/jekyll-typescript-webpack.git" master:gh-pages > /dev/null 2>&1
 else
-  echo 'Travis doesn\t get to push it :('
+  echo "Travis doesn't get to push it :("
   exit 1
 fi
